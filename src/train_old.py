e461b2b902b0945a2c6818d4d8e66f3be1993056
yirenli <irene323@126.com>
2019-05-16 13:59:01 +0200

    failed to solve NaN, train_old added

---
+++ src/train_old.py
@@ -0,0 +1,100 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torch.utils.data import DataLoader, Dataset
+from triplet_net import Net, NetDataset, Net2
+
+
+class Config():
+    margin = 11.3137
+    patch_dir = '/home/yirenli/data/liberty/'
+    read_dir = '../resources/merge_200000_train.txt'
+    write_dir = '../out/net2_tanh_margin{}/net2_tanh_margin{}_merge_200000_train_epoch{}.pkl'
+    pkl_dir = '../out/net2_tanh_margin11.3137/net2_tanh_margin11.3137_merge_200000_train_epoch40.pkl'
+    write_bool = True
+    train_number_epochs = 40
+    patch_len = 43515
+    batchsize = 16
+    size_average = True
+
+
+class TripletLoss(torch.nn.Module):
+    def __init__(self, margin=Config.margin):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    # The distance from the baseline (anchor) input to the positive (truthy) input is minimized,
+    # and the distance from the baseline (anchor) input to the negative (falsy) input is maximized.
+    def forward(self, anchor, positive, negative, size_average=True):
+        distance_positive = (anchor - positive).pow(2).sum(1).pow(.5)
+        distance_negative = (anchor - negative).pow(2).sum(1).pow(.5)
+        # print('p:',distance_positive)
+        # print('n:',distance_negative)
+        # if (anchor - positive).pow(2).sum(1).min()<=0 or (anchor - negative).pow(2).sum(1).min()<=0:
+        # print((anchor - positive).pow(2).sum(1))
+        # print((anchor - negative).pow(2).sum(1))
+        losses = F.relu(distance_positive - distance_negative + self.margin)
+        return losses.mean() if size_average else losses.sum()
+
+
+if __name__ == '__main__':
+    net_dataset = NetDataset(Config)
+    train_dataloader = DataLoader(net_dataset, shuffle=False, batch_size=Config.batchsize)#, num_workers=4)
+    net = Net2().cuda()
+    net.load_state_dict(torch.load(Config.pkl_dir))
+    criterion = TripletLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.00005)
+
+    counter = []
+    loss_history = []
+    iteration_number = 0
+    loss = 0.0
+
+    for epoch in range(0, Config.train_number_epochs):
+        loss = 0.0
+        for i, data in enumerate(train_dataloader, 0):
+            optimizer.zero_grad()
+            img0, img1, img2 = data
+            img0, img1, img2 = img0.cuda(), img1.cuda(), img2.cuda()
+
+            output1, output2, output3 = net(img0, img1, img2)
+            # if i % 20 == 0:
+            #     print(output1)
+            #     print(output2)
+            #     print(output3)
+            loss_triplet = criterion.forward(output1, output2, output3)
+
+            loss_one = loss_triplet.cpu().detach().numpy().sum()
+            if np.isnan(loss_one):
+                torch.save(net.state_dict(),
+                           (Config.write_dir.rstrip('epoch{}.pkl')+'loss2.jpg').format(Config.margin, Config.margin))
+                print('loss_one is nan')
+            else:
+                loss += loss_one
+            # print(loss)
+            loss_triplet.backward()
+
+            # loss += float(loss_triplet)
+            # # print(float(loss_triplet))
+            # loss_triplet.backward()
+
+            optimizer.step()
+            if i % 256 == 255:
+                print('i: {}'.format(i))
+                print('current loss: {}'.format(loss))
+            if (i == (int((Config.patch_len-1) / Config.batchsize))):  # ((i % Config.patch_len) == (Config.patch_len - 1)):
+                iteration_number += Config.patch_len
+                print("Epoch number {}\n Current loss {}\n".format(epoch+40, loss / Config.patch_len))
+                counter.append(iteration_number)
+                loss_history.append(loss / Config.patch_len)
+                if Config.write_bool and (epoch%10)==0:
+                    torch.save(net.state_dict(), (Config.write_dir.format(Config.margin, Config.margin, epoch+40)))
+
+    plt.plot(counter, loss_history)
+    # plt.show()
+    plt.savefig((Config.write_dir.rstrip('epoch{}.pkl')+'loss2.jpg').format(Config.margin, Config.margin))
+    torch.save(net.state_dict(), Config.write_dir.format(Config.margin, Config.margin, Config.train_number_epochs))
--- src/triplet_net.py
+++ src/triplet_net.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import torch.nn as nn
 from torch.utils.data import Dataset
+from multiprocessing import Lock
 
 
 def load_image(p, conf):
@@ -15,6 +16,9 @@ def load_image(p, conf):
     img0l = np.array(img0l, dtype=np.float32)
     npimg0l = img0l.reshape((1, 64, 64))
     return npimg0l
+
+
+lock = Lock()
 
 
 class NetDataset(Dataset):
@@ -32,26 +36,33 @@ class NetDataset(Dataset):
         # print(np.array(self.patches))
         with open(self.conf.read_dir, 'r') as f:
             for l in f:
-                if len(self.patches)>=self.conf.patch_len:
+                if len(self.patches) >= self.conf.patch_len:
                     break
                 a = l.strip('\n').split(' ')
                 line = [a[0], a[1], a[2]]
                 self.patches.append(line)
-        for p in self.patches:
-            if not p[0] in self.dataset.keys():
-                self.dataset[p[0]] = load_image(p[0],self.conf)
-            if not p[1] in self.dataset.keys():
-                self.dataset[p[1]] = load_image(p[1],self.conf)
-            if not p[2] in self.dataset.keys():
-                self.dataset[p[2]] = load_image(p[2], self.conf)
-            print(self.dataset.__len__())
+        # for p in self.patches:
+        #     if not p[0] in self.dataset.keys():
+        #         self.dataset[p[0]] = load_image(p[0],self.conf)
+        #     if not p[1] in self.dataset.keys():
+        #         self.dataset[p[1]] = load_image(p[1],self.conf)
+        #     if not p[2] in self.dataset.keys():
+        #         self.dataset[p[2]] = load_image(p[2], self.conf)
+        #     print(self.dataset.__len__())
 
     def __getitem__(self, item):
         # print(self.patches[self.i])
         self.i += 1
         if self.i == len(self.patches):
             self.i = 0
-        return self.dataset[self.patches[self.i][0]], self.dataset[self.patches[self.i][1]], self.dataset[self.patches[self.i][2]]
+        temp = []
+        for i in range(3):
+            lock.acquire()
+            if not self.patches[self.i][i] in self.dataset.keys():
+                self.dataset[self.patches[self.i][i]] = load_image(self.patches[self.i][i], self.conf)
+            temp.append(self.dataset[self.patches[self.i][i]])
+            lock.release()
+        return temp[0], temp[1], temp[2]
         # imageID0 = int(int(self.patches[self.i][0]) / 256)  # >> 8
         # patchID0 = int(self.patches[self.i][0]) % 256  # & b'11111111
         # image_dir0 = self.conf.patch_dir + ('patches%04d.bmp' % (imageID0))
@@ -162,33 +173,44 @@ class Net(nn.Module):
 class Net2(nn.Module):
     def __init__(self):
         super(Net2, self).__init__()
+        temp_conv = []
+        temp_conv.append(nn.Conv2d(in_channels=1, out_channels=4, kernel_size=3, stride=2, padding=0))
+        temp_conv.append(nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, stride=2, padding=0))
+        temp_conv.append(nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2, padding=0))
+        temp_conv.append(nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=0))
+        temp_conv.append(nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=0))
+        temp_conv.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0))
+        # for tc in temp_conv:
+        #     nn.init.uniform_(tc.weight, a=0, b=0)
+        #     tc.bias.data.fill_(0)
+
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=4, kernel_size=3, stride=2, padding=0),
+            temp_conv[0],
             # nn.BatchNorm2d(4),
             nn.ReLU()
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, stride=2, padding=0),
+            temp_conv[1],
             # nn.BatchNorm2d(8),
             nn.ReLU()
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2, padding=0),
+            temp_conv[2],
             # nn.BatchNorm2d(16),
             nn.ReLU()
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=0),
+            temp_conv[3],
             # nn.BatchNorm2d(32),
             nn.ReLU()
         )
         self.conv5 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=0),
+            temp_conv[4],
             # nn.BatchNorm2d(64),
             nn.ReLU()
         )
         self.conv6 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0),
+            temp_conv[5],
             # nn.BatchNorm2d(128),
             nn.Tanh()
         )
